d257dd7dc27b0c600285ca45c0ab3bdc6994d58d
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2019-11-03 15:28:18 +0100

    upd: updating decode and encode Vigenere by suppressing the key extension function

--- src/alexandreladriere/encryptionmethods/vigenere/Vigenere.kt
+++ src/alexandreladriere/encryptionmethods/vigenere/Vigenere.kt
@@ -3,31 +3,16 @@ package alexandreladriere.encryptionmethods.vigenere
 import alexandreladriere.encryptionmethods.utils.flattenToAscii
 
 /**
- * This function extends the key in a cyclic manner until its length matches the text length
- * @param txt Text to be encrypted
- * @param key Encryption key
- */
-private fun extendKey(txt: String, key: String): String {
-    val txtLength: Int = txt.length
-    val keyLength: Int = key.length
-    var keyExtended = ""
-    for (i in 0 until txtLength) {
-        keyExtended += key[i.rem(keyLength)]
-    }
-    return keyExtended
-}
-
-/**
  * This function encodes the given message with the given key
  * @param txt Text to be encrypted
  * @param key Encryption key
  */
 fun encodeMessage_Vigenere(txt: String, key: String): String {
     var txtEncoded = ""
-    val keyExtended = extendKey(txt, key)
+    val keyLength: Int = key.length
     val txtFlattened = flattenToAscii(txt)
     for (i in txtFlattened.indices) {
-        val x = (VIGENERE_ARRAY.indexOf(txtFlattened[i].toUpperCase()) + VIGENERE_ARRAY.indexOf(keyExtended[i].toUpperCase())).rem(
+        val x = (VIGENERE_ARRAY.indexOf(txtFlattened[i].toUpperCase()) + VIGENERE_ARRAY.indexOf(key[i.rem(keyLength)].toUpperCase())).rem(
             VIGENERE_ARRAY.size)
         txtEncoded += VIGENERE_ARRAY[x]
     }
@@ -41,9 +26,9 @@ fun encodeMessage_Vigenere(txt: String, key: String): String {
  */
 fun decodeMessage_Vigenere(txt: String, key: String): String {
     var txtDecoded = ""
-    val keyExtended = extendKey(txt, key)
+    val keyLength: Int = key.length
     for (i in txt.indices) {
-        val x = (VIGENERE_ARRAY.indexOf(txt[i].toUpperCase()) - VIGENERE_ARRAY.indexOf(keyExtended[i].toUpperCase()) + VIGENERE_ARRAY.size).rem(
+        val x = (VIGENERE_ARRAY.indexOf(txt[i].toUpperCase()) - VIGENERE_ARRAY.indexOf(key[i.rem(keyLength)].toUpperCase()) + VIGENERE_ARRAY.size).rem(
             VIGENERE_ARRAY.size)
         txtDecoded += VIGENERE_ARRAY[x]
     }
